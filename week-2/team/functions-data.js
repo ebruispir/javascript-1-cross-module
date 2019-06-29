7ce59f10d4ebc27fd5ede42920ef96d7183a93a2
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-06-29 16:45:52 +0200

    Update functions-data.js

--- week-2/team/functions-data.js
+++ week-2/team/functions-data.js
@@ -1,8 +1,17 @@
-// data (variables)
-
-
+var teamName ="Javascript";
+var teamMembers =[ "Ebru", "Erdem","Esra" ];
 
 // functions
-
-
-/* onclick's */
+function print_team_name(teamName){
+    console.log("Welcome to the " + teamName + " project.");
+}
+function print_team_members(teamMembers){
+    console.log("My team members are as follows:" );
+    for(var i=0; i < teamMembers.length; i++){
+        console.log(teamMembers[i]);
+    }
+}
+function introduce(teamName,teamMembers){
+     print_team_name(teamName);
+     print_team_members(teamMembers);
+}
