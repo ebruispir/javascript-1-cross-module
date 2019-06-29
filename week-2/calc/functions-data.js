c423fa90b2b960c088503eef244aef1f21251581
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-06-29 19:05:35 +0200

    Update functions-data.js

--- week-2/calc/functions-data.js
+++ week-2/calc/functions-data.js
@@ -1,8 +1,30 @@
 // data (variables)
-
-
-
+var x = 10;
+var y = 5;
 // functions
+function print_vals (a,b){
+console.log("number1: "+ a +"\nnumber2: "+ b);
+}
+
+function add(a,b){
+   return a+b; 
+}
+
+function subtract(a,b){
+   return a-b;     
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
 
 
-/* onclick's */
+function do_math(a,b,func){
+    
+  console.log (func(a,b));
+
+}
