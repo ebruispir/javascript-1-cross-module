f108f6b4d31a763072707b94746562dc0c8e0a39
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-06-29 14:47:19 +0200

    Update scripts.js

--- week-2/scripts.js
+++ week-2/scripts.js
@@ -1,5 +1,13 @@
-// data (variables)
+ // data (variables)
+ var project = { 
+    name:"Javascript-1", 
+    link:"https://github.com/ebruispir/javascript-1-homework",
+    description:"My javascript homework on Github"
+}
+ // functions
 
-
-
-// functions
+ function print_deets(project) {
+    for (let key in project){
+        console.log(key +" : "+ project[key]);
+    }
+} 
