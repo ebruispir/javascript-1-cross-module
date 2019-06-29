ef6a1aa695b45d9db7b2aa002a2a6d762e8c1eb0
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-06-29 16:40:29 +0200

    Update onclicks.js

--- week-2/team/onclicks.js
+++ week-2/team/onclicks.js
@@ -1,1 +1,12 @@
-onclicks.js
+var tname = document.getElementById("team-name");
+tname.onclick=function(){
+    print_team_name(teamName);
+}
+var tmember = document.getElementById("team-member" );
+tmember.onclick=function(){
+    print_team_members(teamMembers);
+}
+var int = document.getElementById("intro");
+int.onclick=function(){
+    introduce(teamName,teamMembers);
+}
