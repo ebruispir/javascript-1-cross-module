88c32db4ef0d047d6373f499b92c11cf96ba0b2b
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-07-05 01:45:38 +0200

    Update data-&-functions.js

--- week-3/calc/data-&-functions.js
+++ week-3/calc/data-&-functions.js
@@ -1,1 +1,30 @@
+// data (variables)
+var a = 10;
+var b = 5;
+// functions
+function print_vals (a,b){
+console.log("number1: "+ a +"\nnumber2: "+ b);
+}
 
+function add(a,b){
+   return a+b; 
+}
+
+function subtract(a,b){
+   return a-b;     
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+function do_math(a,b,func){
+    
+  console.log (func(a,b));
+
+}
