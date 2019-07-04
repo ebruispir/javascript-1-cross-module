276b5b9de61f0294ab4706fdcddc04704f8e677f
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-07-05 01:50:48 +0200

    Update data-&-functions.js

--- week-3/data-&-functions.js
+++ week-3/data-&-functions.js
@@ -1,1 +1,16 @@
+ // data (variables)
+ var project = { 
+    name:"Javascript-1", 
+    link:"https://github.com/ebruispir/javascript-1-homework",
+    description:"My javascript homework on Github"
+}
+ // functions
+
+ function print_deets(project) {
+    for (let key in project){
+        console.log(key +" : "+ project[key]);
+    }
+} 
+
+
 
