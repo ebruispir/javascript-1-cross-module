9c4e92dcdc85835a5103364182aca192b1eb1b51
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-07-05 01:49:36 +0200

    Update handlers-&-listeners.js

--- week-3/handlers-&-listeners.js
+++ week-3/handlers-&-listeners.js
@@ -1,1 +1,9 @@
+/*   handlers  */
+function print_handler(){
+    print_deets(project);
+} 
 
+/*   event listeners  */
+
+var print = document.getElementById("print");
+print.addEventListener("click",print_handler);
